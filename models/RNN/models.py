c49827b5a56d183bbf4fa0f7468d2aba016b7843
JadeLee274 <tmdals274@snu.ac.kr>
2025-02-24 14:08:30 +0900

    models.py modify

--- models/RNN/models.py
+++ models/RNN/models.py
@@ -5,8 +5,10 @@ from typing import *
 def sigmoid(x: np.ndarray) -> np.ndarray:
     return (np.exp(x) / sum(np.exp(x)))
 
+
 def tanh(x: np.ndarray) -> np.ndarray:
     return np.tanh(x)
+
 
 def softmax(x: np.ndarray) -> np.ndarray:
     return np.exp(x) / np.sum(np.exp(x))
