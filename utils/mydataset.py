f0e58b2521eb51ec74ea96de3cdc0287cfdc3234
JadeLee274 <tmdals274@snu.ac.kr>
2025-04-03 19:04:05 +0900

    merge dataset files

--- utils/mydataset.py
+++ utils/mydataset.py
@@ -34,3 +34,24 @@ class TensorDataset(Dataset):
         if self.transform:
             data = self.transform(data)
         return data
+
+
+class IMDBDataset(Dataset):
+    def __init__(
+            self,
+            texts: List[int],
+            labels: List[int],
+    ) -> None:
+        self.texts = torch.tensor(texts, dtype=torch.long)
+        self.labels = torch.tensor(labels, dtype=torch.long)
+
+    def __len__(
+            self,
+    ) -> int:
+        return len(self.texts)
+    
+    def __getitem__(
+            self,
+            idx: int,
+    ) -> Tuple[List[int], List[int]]:
+        return self.texts[idx], self.texts[idx]
