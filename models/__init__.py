b90391375fdd9dca9c7d9ad73fb219056595ef81
JadeLee274 <tmdals274@snu.ac.kr>
2025-04-03 19:18:39 +0900

    add __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+from resnet.Resnet import ResNet, ResNet50, ResNet101, ResNet152
+from vae.vae import VAE
+from gan.gan import Generator, Discriminator
+
+__all__ = [
+    ResNet,
+    ResNet50,
+    ResNet101,
+    ResNet152,
+    VAE,
+    Generator,
+    Discriminator
+]
